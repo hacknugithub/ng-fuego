8f1ec023a48845cb9c778a654c71f93758dc32d8
hacknugithub <manu_pacu@hotmail.com>
2018-03-19 00:15:38 -0600

    added routing module, auth components, auth service and connected to sign in a user with fb

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '../material.module';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
+  exports: [CommonModule, FormsModule, MaterialModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, MaterialModule, ReactiveFormsModule],
   declarations: []
 })
 export class SharedModule { }
