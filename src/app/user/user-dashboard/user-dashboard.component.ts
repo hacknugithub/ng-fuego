1e4aea39c307918328b3681b29569f4386969fa9
hacknugithub <manu_pacu@hotmail.com>
2018-03-19 23:34:41 -0600

    Added fields to update user and also change/upload photo

--- src/app/user/user-dashboard/user-dashboard.component.ts
+++ src/app/user/user-dashboard/user-dashboard.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {Location} from '@angular/common';
 import { User } from '../user.model';
+import { AngularFireStorage, AngularFireUploadTask } from 'angularfire2/storage';
 import {AuthService} from '../../core/auth.service';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-user-dashboard',
@@ -11,7 +14,14 @@ export class UserDashboardComponent implements OnInit {
   editing = false;
   user: User;
 
-  constructor(private auth: AuthService) { }
+  task: AngularFireUploadTask
+
+  constructor(
+    private auth: AuthService, 
+    private userService: UserService,
+    private storage: AngularFireStorage,
+    private location: Location
+  ) { }
 
   ngOnInit() {
     this.getUser()
@@ -21,4 +31,40 @@ export class UserDashboardComponent implements OnInit {
     return this.auth.user.subscribe(user => (this.user = user))
   }
 
+  updateProfile() {
+    return this.userService.updateProfileData(
+      this.user.displayName,
+      this.user.photoURL
+    )
+  }
+
+  updateEmail(){
+    return this.userService.updateEmailData(this.user.email)
+  }
+
+  uploadPhotoURL(event): void {
+    const file = event.target.files[0]
+    const path = `users/${this.user.uid}/photos/${file.name}`
+    if(file.type.split('/')[0] !== 'image') {
+      return alert('only images allowed')
+    } else {
+      this.task = this.storage.upload(path, file)
+      this.task.downloadURL().subscribe(url => {
+        this.userService.updateProfileData(this.user.displayName, url)
+      })
+    }
+  }
+
+  updateUser() {
+    const data = {
+      website: this.user.website || null,
+      location: this.user.location || null,
+      bio: this.user.bio || null,
+    }
+    return this.userService.updateUserData(data)
+  }
+
+  goBack() {
+    this.location.back()
+  }
 }
