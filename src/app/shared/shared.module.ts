f08fadb2b7e3b62005f9625a6bc55bac48b97493
hacknugithub <manu_pacu@hotmail.com>
2018-03-22 16:50:43 -0600

    pipe added to post list using momentjs

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '../material.module';
+import { FromNowPipe } from './from-now.pipe';
 
 @NgModule({
-  exports: [CommonModule, FormsModule, MaterialModule, ReactiveFormsModule],
   imports: [CommonModule, FormsModule, MaterialModule, ReactiveFormsModule],
-  declarations: []
+  exports: [CommonModule, FormsModule, MaterialModule, ReactiveFormsModule, FromNowPipe],
+  declarations: [FromNowPipe]
 })
 export class SharedModule { }
